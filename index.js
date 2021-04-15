e8325c8d17393eb642c1fe903ecc88a388f50e31
HybridMoment <rogersgeric@gmail.com>
2021-04-15 11:14:14 -0700

    all routes should work now. Tested with Postman

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = new express()
 app.use('/api', router)
 app.use(multer)
 mongoose.connect(MONGO_URI, {useNewUrlParser: true , useUnifiedTopology: true}) 
+mongoose.set('returnOriginal', false);
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'));
 
--- routes/router.js
+++ routes/router.js
@@ -7,60 +7,80 @@ const router = express.Router()
 
 //get request to return ALL bikes
 router.get('/getbikes', async (request, response) => {
+    
     try {
         const allBikes = await Bike.find()
         response.json(allBikes)
+
     } catch (error) {
         response.status(500).json({ message : error.message})
     }
+
 })
 
 //get request to return a specific bike
-router.get('/findbike', upload.none() ,async (request, response) => {
+router.get('/findbike', upload.none(), async (request, response) => {
+    
     try {
         const bike = await Bike.findOne({bikeID : request.body.Bike_ID}) //Could this input be a source of error? 
         response.json(bike)
+
     } catch (error) {
         response.status(500).json({ message : error.message})
     }
+
 })
 
 //patch request to update bike repair information
-router.patch('/fixbike', async (request, response) => {
+router.patch('/fixbike', upload.none(), async (request, response) => {
+    
     try {
-        
+        const updatedBike = await Bike.findOneAndUpdate(
+            {bikeID : request.body.Bike_ID}, 
+            {$push : {repairHistory : request.body.repair}})
+        response.json(updatedBike)
+
     } catch (error) {
         response.status(500).json({ message : error.message})
     }
+
 })
 
 //post request to add a bike 
-router.post('/addBike', async (request, response) => {
+router.post('/addBike', upload.none(), async (request, response) => {
+    
     try {
         let bike = new Bike({
-            bikeID : request.body.bikeID,
+            bikeID : request.body.Bike_ID,
             repairHistory : ["N/A"],
             active : true ,
             recall : false
         })
         await bike.save()
-        
+        response.json(bike)
+
     } catch (error) {
         response.status(500).json({ message : error.message})
     }
+
 })
 
 //delete request when decomissioning a bike
-router.delete('/decomissionbike', async (request, response) => {
+router.delete('/decomissionbike', upload.none(), async (request, response) => {
+    
     try {
-        
+        const removedBike = await Bike.findOneAndDelete({bikeID : request.body.Bike_ID})
+        response.json(removedBike)
+
     } catch (error) {
         response.status(500).json({ message : error.message})
     }
+
 })
 
 //post request to add employee 
-router.post('/addemployee', async (request, response) => {
+router.post('/addemployee', upload.none(), async (request, response) => {
+    
     try {
         let employee = new Employee({
             name : request.body.Name,
@@ -70,19 +90,25 @@ router.post('/addemployee', async (request, response) => {
             position : request.body.Position
         })
 
+        response.json(employee)
+
     } catch (error) {
         response.status(500).json({ message : error.message})
     }
+
 })
 
 //get request to return all employees
 router.get('/getemployees', async (request, response) => {
+    
     try {
         const allEmployees = await Employee.find()
         response.json(allEmployees)
+
     } catch (error) {
         response.status(500).json({ message : error.message})
     }
+
 })
 
 //
